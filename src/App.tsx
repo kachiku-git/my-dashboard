45d80f61481bc13b3425204ce796a48bcfbf6a22
kachiku-git <fuji0syaru0@gmail.com>
2025-11-25 09:31:53 +0900

    11/25 静的カンプの作成完成させる

--- src/App.tsx
+++ src/App.tsx
@@ -52,24 +52,39 @@ function App() {
           </div>
 
           <div className="task-content">
-
             <div className="widget-card">
               <h2 className="widget-title">ここにタスク一覧を表示させる</h2>
               <div className="task-wrap">
                 <ul className="task-lists">
                   <li className="task-item">
-                    <button className="option-button">追加</button>
-                    <button className="option-button">編集</button>
-                    <button className="option-button">削除</button>
+                    <div className="button-wrap">
+                      <button className="option-button">追加</button>
+                      <button className="option-button">編集</button>
+                      <button className="option-button">削除</button>
+                    </div>
                     <p className="task">
                       タスクの追加・編集・削除ができるようにする
                     </p>
                   </li>
                   <li className="task-item">
-                    <button className="option-button">編集</button>
-                    <button className="option-button">削除</button>
-                    <button className="option-button">追加</button>
+                    <div className="button-wrap">
+                      <button className="option-button">追加</button>
+                      <button className="option-button">編集</button>
+                      <button className="option-button">削除</button>
+                    </div>
                     <p className="task">
+                      タスクの追加・編集・削除ができるようにする
+                    </p>
+                  </li>
+                  <li className="task-item">
+                    <div className="button-wrap">
+                      <button className="option-button">追加</button>
+                      <button className="option-button">編集</button>
+                      <button className="option-button">削除</button>
+                    </div>
+                    <p className="task">
+                      タスクの追加・編集・削除ができるようにする
+                      タスクの追加・編集・削除ができるようにする
                       タスクの追加・編集・削除ができるようにする
                     </p>
                   </li>
